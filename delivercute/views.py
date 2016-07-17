8dba92cdeb88c35864c44ab87ea1be6f0a5a7f65
Will Weatherford <weatherford.william@gmail.com>
2016-07-17 01:45:16 -0700

    Using CreateView to show new Subscriber entry form on homepage.

--- delivercute/views.py
+++ delivercute/views.py
@@ -1,7 +1,11 @@
-from django.views.generic import TemplateView
+from django.views.generic import CreateView
+from subscribers.models import Subscriber
 
-class Main(TemplateView):
+
+class Main(CreateView):
     template_name = 'main.html'
+    model = Subscriber
+    fields = ['email', 'send_hour']
     # load a form - CreateView?
     # save or update preferences
     # modelview
