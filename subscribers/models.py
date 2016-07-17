98e4ef5ed32f77da5a2c537287a1bf11bb08ebf9
Will Weatherford <weatherford.william@gmail.com>
2016-07-17 01:27:52 -0700

    Created and migrated Subscriber model.

--- subscribers/models.py
+++ subscribers/models.py
@@ -1,3 +1,11 @@
+"""Subscriber model tracking preferences for each email."""
 from django.db import models
 
-# Create your models here.
+HOUR_CHOICES = [(n, str(n) + ':00') for n in range(24)]
+
+
+class Subscriber(models.Model):
+    """Subscriber model tracking preferences for each email."""
+
+    email = models.EmailField()
+    send_hour = models.IntegerField(choices=HOUR_CHOICES, default=8)
