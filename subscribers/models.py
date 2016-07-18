c5c9f6a24d8871e21c244f9a9185dae535bca5ae
Will Weatherford <weatherford.william@gmail.com>
2016-07-18 13:18:59 -0700

    get_hour function more succinct.

--- subscribers/models.py
+++ subscribers/models.py
@@ -4,9 +4,7 @@ from django.db import models
 
 def get_hour(n):
     """Convert from 24-hour number range to 12-hour AM/PM format."""
-    hour_num = n % 12 or 12
-    ampm = 'AM' if n < 12 else 'PM'
-    return (n, '{}:00 {}'.format(hour_num, ampm))
+    return (n, '{}:00 {}'.format(n % 12 or 12, 'AM' if n < 12 else 'PM'))
 
 HOUR_CHOICES = map(get_hour, range(24))
 
