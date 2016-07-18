7a7dfcbe080b46a599cdcda397dbb61243887012
Will Weatherford <weatherford.william@gmail.com>
2016-07-18 00:53:56 -0700

    Improved HOUR_CHOICES creation to give 12 AM through 11 PM.

--- subscribers/models.py
+++ subscribers/models.py
@@ -1,11 +1,16 @@
 """Subscriber model tracking preferences for each email."""
 from django.db import models
+from itertools import product
 
-HOUR_CHOICES = [(n, str(n) + ':00') for n in range(24)]
+HOUR_CHOICES = [(num, '{}:00 {}'.format(num, am_pm))
+                for am_pm, num in product(
+                    ('AM', 'PM'),
+                    [12] + list(range(1, 12)))
+                ]
 
 
 class Subscriber(models.Model):
     """Subscriber model tracking preferences for each email."""
 
-    email = models.EmailField()
+    email = models.EmailField(unique=True)
     send_hour = models.IntegerField(choices=HOUR_CHOICES, default=8)
