d04a576d079570eec24eba83b9ad363615994169
Will Weatherford <weatherford.william@gmail.com>
2016-07-18 13:18:11 -0700

    Fixed hour choices to properly work with datetime 24-hour number.

--- subscribers/models.py
+++ subscribers/models.py
@@ -1,12 +1,14 @@
 """Subscriber model tracking preferences for each email."""
 from django.db import models
-from itertools import product
 
-HOUR_CHOICES = [(num, '{}:00 {}'.format(num, am_pm))
-                for am_pm, num in product(
-                    ('AM', 'PM'),
-                    [12] + list(range(1, 12)))
-                ]
+
+def get_hour(n):
+    """Convert from 24-hour number range to 12-hour AM/PM format."""
+    hour_num = n % 12 or 12
+    ampm = 'AM' if n < 12 else 'PM'
+    return (n, '{}:00 {}'.format(hour_num, ampm))
+
+HOUR_CHOICES = map(get_hour, range(24))
 
 
 class Subscriber(models.Model):
