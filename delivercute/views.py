c7925ad7da4ee43789128e6735d61e10165074c2
Will Weatherford <weatherford.william@gmail.com>
2016-07-18 11:09:57 -0700

    Working form_valid() method of main view updates if subscriber object already exists.

--- delivercute/views.py
+++ delivercute/views.py
@@ -1,5 +1,6 @@
-from django.views.generic import CreateView
 from subscribers.models import Subscriber
+from django.views.generic import CreateView
+from django.http import HttpResponseRedirect
 
 
 class Main(CreateView):
@@ -9,13 +10,11 @@ class Main(CreateView):
     success_url = '/'
 
     def form_valid(self, form):
-        # utilizing form_valid of ModelFormMixin
-
         data = form.cleaned_data
         instance, created = Subscriber.objects.update_or_create(
             email=data.get('email', ''),
-            defaults=data)
-        import pdb;pdb.set_trace()
+            defaults=data,
+        )
         instance.save()
         self.object = instance
-        return super(CreateView, self).form_valid(form)
+        return HttpResponseRedirect(self.get_success_url())
