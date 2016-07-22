0f50a50fc8890b331b505209db937dc0ba26ecef
Will Weatherford <weatherford.william@gmail.com>
2016-07-22 13:48:34 -0700

    Added on_deploy.py to be run on deployment.

---
+++ on_deploy.py
@@ -0,0 +1,35 @@
+"""Run commands upon deployment to set up admin user and initial subreddits."""
+
+import os
+import sys
+from django.contrib.auth.models import User
+from subscribers.models import SubReddit
+
+SUBREDDIT_NAMES = [
+    'StartledCats',
+    'kittengifs',
+    'gifsofotters',
+    'Eyebleach',
+    'babyelephantgifs',
+    'babybigcatgifs',
+    'awwgifs',
+    'AnimalsBeingConfused',
+    'AnimalsBeingDerps',
+    'AnimalsBeingBros',
+    'aww',
+    'rarepuppers',
+]
+
+try:
+    EMAIL = os.environ['DELIVERCUTE_EMAIL']
+    PASSWORD = os.environ['DELIVERCUTE_PASSWORD_BASIC']
+except KeyError:
+    print('Global security variables not set.')
+    sys.exit()
+
+superuser = User.objects.create_superuser('delivercute', EMAIL, PASSWORD)
+superuser.save()
+
+for subreddit_name in SUBREDDIT_NAMES:
+    new = SubReddit(display_name=subreddit_name)
+    new.save()
