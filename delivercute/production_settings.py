16b3b18b770653492a0a9776f98b1592753d4dd6
Will Weatherford <weatherford.william@gmail.com>
2016-07-26 16:51:57 -0700

    Modified allowed_domains with dot-first syntax.

--- delivercute/production_settings.py
+++ delivercute/production_settings.py
@@ -4,7 +4,7 @@ from delivercute.settings import *
 DEBUG = False
 ALLOWED_HOSTS.extend(
     ['.us-west-2.compute.amazonaws.com',
-     'will-weatherford.com',
+     '.will-weatherford.com',
      ]
 )
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
