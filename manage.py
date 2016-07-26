862deab3a202a33f2597c467e4e7f17349961f79
Will Weatherford <weatherford.william@gmail.com>
2016-07-25 18:49:59 -0700

    Specifying using python3.5 for manage.py

--- manage.py
+++ manage.py
@@ -1,9 +1,8 @@
-#!/usr/bin/env python
+#!/usr/bin/python3.5
 # import os
 import sys
 
 if __name__ == "__main__":
-    # os.environ.setdefault("DJANGO_SETTINGS_MODULE", "delivercute.settings")
 
     from django.core.management import execute_from_command_line
 
