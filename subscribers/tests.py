de6c2e58208dd9944330cb502f2ad9eee31f63eb
Will Weatherford <weatherford.william@gmail.com>
2016-07-28 13:32:54 -0700

    Added testing factories for SubReddit and Subscriber.

--- subscribers/tests.py
+++ subscribers/tests.py
@@ -1,7 +1,33 @@
+"""Testing Subscriber and SubReddit models."""
+
 from django.test import TestCase
-from .models import Subscriber
+from .models import Subscriber, SubReddit
+import factory
 
 DUMMY_EMAIL = 'example@example.com'
+
+
+class SubRedditFactory(factory.django.DjangoModelFactory):
+    """Creates SubReddit models for testing."""
+
+    class Meta:
+        """Assign SubReddit model as product of factory."""
+
+        model = SubReddit
+
+    email = factory.Faker('email')
+
+
+class SubscriberFactory(factory.django.DjangoModelFactory):
+    """Creates Subscriber models for testing."""
+
+    class Meta:
+        """Assign Subscriber model as product of factory."""
+
+        model = Subscriber
+
+    email = factory.Faker('email')
+    
 
 
 class SimpleCase(TestCase):
