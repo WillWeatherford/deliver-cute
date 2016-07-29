8c826db53d809f916f630a0d82c57e8ac0eb8e6f
Will Weatherford <weatherford.william@gmail.com>
2016-07-29 09:05:35 -0700

    Changed test dir structure.

---
+++ constants.py
@@ -0,0 +1,16 @@
+"""Hold constant values used in multiple places in project."""
+
+SUBREDDIT_NAMES = [
+    'StartledCats',
+    'kittengifs',
+    'gifsofotters',
+    'Eyebleach',
+    'babyelephantgifs',
+    'babybigcatgifs',
+    'awwgifs',
+    'AnimalsBeingConfused',
+    'AnimalsBeingDerps',
+    'AnimalsBeingBros',
+    'aww',
+    'rarepuppers',
+]
