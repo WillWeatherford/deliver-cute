7fde8d96e85fff6501b1ba95e40eeb5e38285b30
Will Weatherford <weatherford.william@gmail.com>
2016-07-29 16:35:26 -0700

    Working parameterized unittests on url-matching regex.

--- test_scripts.py
+++ test_scripts.py
@@ -5,11 +5,11 @@ import random
 import factory
 from string import ascii_letters, digits
 from itertools import product
-from on_schedule import gather_posts, fix_image_links
+from on_schedule import gather_posts, fix_image_links, SRC_PAT
 from constants import SUBREDDIT_NAMES, LIMIT
 from django.test import TestCase
-from .models import Subscriber, SubReddit
-from factory.django import DjangoModelFactory
+# from subscribers.models import Subscriber, SubReddit
+from nose_parameterized import parameterized
 
 # TODO
 # test htmlization
@@ -30,43 +30,52 @@ BODY = '''
 
 PROTO = ('http://', 'https://')
 DOMAIN = ('i.imgur.com/', )
-HASH = ''.join(random.choice(ascii_letters + digits) for _ in range(8))
+HASH = ''.join(random.sample(ascii_letters + digits, 8))
 EXT = ('.jpg', '.gifv', '.gif', '.png', )
 
 BAD_DOMAIN = ('www.imgur.com/', 'reddit.com/', 'gfycat.com/')
 BAD_EXT = ('', )
+
+GOOD_URLS = product(PROTO, DOMAIN, HASH, EXT)
+BAD_URLS = product(PROTO, BAD_DOMAIN, HASH, BAD_EXT)
 
 
 class RedditAPICase(TestCase):
     """Test retrieval of posts from reddit API."""
 
     def setUp(self):
+        """Get post data to test."""
         self.posts = list(gather_posts(SUBREDDIT_NAMES, LIMIT))
 
 
+class CheckURLCase(TestCase):
+    """Test URL matching regexes."""
 
-@pytest.fixture(params=product(PROTO, DOMAIN, HASH, EXT))
-def good_src_url(request):
-    """Generate good imgur source urls."""
-    return ''.join(request.param)
+    @parameterized.expand((url, ) for url in GOOD_URLS)
+    def test_src_pat_good(self, url):
+        """Confirm that link regex works as expected for good urls."""
+        self.assertIsNotNone(SRC_PAT.match(''.join(url)))
+
+    @parameterized.expand((url, ) for url in BAD_URLS)
+    def test_src_pat_bad(self, url):
+        """Confirm that link regex works as expected for bad urls."""
+        self.assertIsNone(SRC_PAT.match(''.join(url)))
+
+    # def test_cute_links_source(fixed_link):
+    #     """Confirm that links fixed by source fixer match the expected pattern."""
+    #     assert SRC_PAT.match(fixed_link.url) is not None
 
 
-@pytest.fixture(params=product(PROTO, BAD_DOMAIN, HASH, BAD_EXT))
-def bad_src_url(request):
-    """Generate bad source urls."""
-    return ''.join(request.param)
+# @pytest.fixture(params=CUTE_POSTS)
+# def cute_post(request):
+#     """Generate cute links directly from source."""
+#     return request.param
 
 
-@pytest.fixture(params=CUTE_POSTS)
-def cute_post(request):
-    """Generate cute links directly from source."""
-    return request.param
-
-
-@pytest.fixture(params=FIXED_LINKS)
-def fixed_link(request):
-    """Generate link fixed by having correct source url."""
-    return request.param
+# @pytest.fixture(params=FIXED_LINKS)
+# def fixed_link(request):
+#     """Generate link fixed by having correct source url."""
+#     return request.param
 
 
 # def test_emai():
@@ -89,39 +98,24 @@ def fixed_link(request):
 #     send_email(WILL_EMAIL, WILL_EMAIL, [WILL_EMAIL], 'ESCAPE TEST', body)
 
 
-def test_cute_posts(cute_post):
-    """Test generating links."""
-    assert cute_post.url.startswith('http')
+# def test_cute_posts(cute_post):
+#     """Test generating links."""
+#     assert cute_post.url.startswith('http')
 
 
-def test_cute_posts_count():
-    """Test that number of links is at or under the limit per subreddit."""
-    assert len(CUTE_POSTS) <= len(SUBREDDIT_NAMES) * LIMIT
+# def test_cute_posts_count():
+#     """Test that number of links is at or under the limit per subreddit."""
+#     assert len(CUTE_POSTS) <= len(SUBREDDIT_NAMES) * LIMIT
 
 
-def test_cute_posts_dupes():
-    """Test that number of links is at or under the limit per subreddit."""
-    from on_schedule import dedupe_posts
-    duplicated_posts = CUTE_POSTS + CUTE_POSTS
-    deduplicated_urls = [post.url for post in dedupe_posts(duplicated_posts)]
-    deduplicated_urls = list(sorted(deduplicated_urls))
-    urls = list(sorted(set(post.url for post in CUTE_POSTS)))
-    assert deduplicated_urls == urls
+# def test_cute_posts_dupes():
+#     """Test that number of links is at or under the limit per subreddit."""
+#     from on_schedule import dedupe_posts
+#     duplicated_posts = CUTE_POSTS + CUTE_POSTS
+#     deduplicated_urls = [post.url for post in dedupe_posts(duplicated_posts)]
+#     deduplicated_urls = list(sorted(deduplicated_urls))
+#     urls = list(sorted(set(post.url for post in CUTE_POSTS)))
+#     assert deduplicated_urls == urls
 
 
-def test_src_pat_good(good_src_url):
-    """Confirm that link regex works as expected for good urls."""
-    from on_schedule import SRC_PAT
-    assert SRC_PAT.match(good_src_url) is not None
 
-
-def test_src_pat_bad(bad_src_url):
-    """Confirm that link regex works as expected for bad urls."""
-    from on_schedule import SRC_PAT
-    assert SRC_PAT.match(bad_src_url) is None
-
-
-def test_cute_links_source(fixed_link):
-    """Confirm that links fixed by source fixer match the expected pattern."""
-    from on_schedule import SRC_PAT
-    assert SRC_PAT.match(fixed_link.url) is not None
